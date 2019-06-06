04f7bd5e934478252b6241adbbe0b29832ab910c
jkMLnop <konradkarolak87@gmail.com>
2019-06-06 13:14:08 -0400

    Adding Updates to Scraper - successfully pulling all model makes and years

--- scrape_fuelly.py
+++ scrape_fuelly.py
@@ -2,34 +2,36 @@ import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
+#Pulls links for every year of the current model and store in dict
+def model_years(car_model_link):
+    #TODO do I need seperate UA or can I pass it in?
+    year_ua = UserAgent()
+    year_page = requests.get(car_model_link,headers={'user-agent':year_ua.chrome})
+    year_soup = BeautifulSoup(year_page.content,'lxml')
+
+    uls = year_soup.find_all('ul', class_ = "model-year-summary")
+
+    all_model_year_links = [ul.get('data-clickable') for ul in uls]
+
+    print(all_model_year_links)
+    #TODO START HERE: Why are there empty dicts sometimes?
+    #TODO   write next function for each model year to store all fuel data for a
+    #       model year to a file
 
 user_agent = UserAgent()
 main_url = 'http://www.fuelly.com/car'
 page = requests.get(main_url,headers={'user-agent':user_agent.chrome})
 soup = BeautifulSoup(page.content,'lxml')
 
-all_h4s = soup.find_all('h4',class_='make-header')
+all_divs = soup.find_all('div', class_ = "col-sm-12 col-md-12")
 
-# Stores all links (h4.a['href']) into a dictionary for ease of access
-all_links = [h4.a['href'] for h4 in all_h4s]
+#NOTE do this to get to the second div which contains car model info
+div_container = all_divs[1]
 
-#TODO 1) pull the links for all models from this page as it is much cleaner!
-print(all_links)
+#pull all possible car model page links and store in dict
+all_car_model_links = [atag.get('href') for atag in div_container.find_all('a')]
 
-#TODO 2) do this for each model page
-'''
-# Loop through Car Model page to find all model years
-for link in all_links: #TODO change to full dataset later
-    inner_page = requests.get(link, headers={'user-agent':user_agent.chrome})
-    inner_soup = BeautifulSoup(inner_page.content,'lxml')
+#print(all_car_model_links)
 
-    print(link)
-
-    all_uls = inner_soup.find('ul',class_='model-year-summary')
-
-    #all_years = [ul['data-clickable'] for ul in all_uls]
-
-    print(all_uls)
-
-    #print(all_years)
-'''
+for car_model_link in all_car_model_links:
+    model_years(car_model_link)
