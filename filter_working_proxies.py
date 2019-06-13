a267b8ad4c21dedca6a67687a582c8aa6bcdfda7
jkMLnop <konradkarolak87@gmail.com>
2019-06-13 07:11:38 -0400

    Tool to scrape SSLproxies.org and extract list of proxy ports and IPs, test each individual proxy's ip/port to determine if still active, then write currently active proxies to CSV file.

--- filter_working_proxies.py
+++ filter_working_proxies.py
@@ -28,18 +28,18 @@ def test_proxies(proxy_list):
 
     #TODO ADRI SAYS: Change this to go through the proxy list instead of using index - python not optimized for iterating with indexes!
     #for current_proxy in proxy_list:
-    for n in range (1, (len(proxy_list)-1)):    #TODO possibly fix this? index out of bounds error encountered towards the end of it
+    for n in range (len(proxy_list)-1):    #TODO possibly fix this? index out of bounds error encountered towards the end of it
         current_proxy = proxy_list[n]
         proxy_info = {'http': 'http://'+ current_proxy['ip'] + ':' + current_proxy['port']}
 
         #Test each SSL proxy 10x to see if its still active
-        for n in range(1,10):    #TODO Get Feedback, 1) is 10 requests good? 2) is using icanhazip even relevant? 3) test on scrape target?
+        for n in range(10):    #TODO Get Feedback, 1) is 10 requests good? 2) is using icanhazip even relevant? 3) test on scrape target?
             try:
                 proxy_ip = requests.get(ip_url, proxies = proxy_info)
                 #TODO ADRI SAYS: append to a new list of working proxies instead of deleting from current
             except: # If error, delete this proxy and find another
                 #TODO add some kind of error logging to better identify when its being rejected/blocked by server
-                print('Proxy ' + current_proxy['ip'] + ':' + current_proxy['port'] + 'deleted.')
+                print('Proxy ' + current_proxy['ip'] + ':' + current_proxy['port'] + ' deleted.')
                 del proxy_list[n]   #TODO ADRI SAYS: remove once you add append to new list functionality above
 
         return  proxy_list
@@ -64,7 +64,7 @@ def write_active_proxy_list(active_proxies):
         print("I/O ERROR: file not found!") #TODO this error doesnt work.. fix it!
 
     return
-#TODO 3) make a write proxy list function to write a list of working proxies to write_file
+
 #TODO 4) will have to update scrapers to read into array a list of working proxies and to rotate through them and remove dead ones also!
 
 proxy_list = scrape_proxies()
