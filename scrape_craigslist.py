612762451a143dab75cd6aaa8936252acd711f86
jkMLnop <konradkarolak87@gmail.com>
2019-06-16 17:52:20 -0400

    added craigslist-specific scraping and CSV writing functionality

--- scrape_craigslist.py
+++ scrape_craigslist.py
@@ -38,13 +38,13 @@ def import_zip_list():
 def scrape_by_location(zip_code, city, current_proxy_number, proxy_count, proxy_list, request_count):
     listings_ua = UserAgent()
     listings_url = 'https://' + "".join(city.split(" ")).lower() + '.craigslist.org/search/cta?postal=' + str(zip_code) #TODO Change for gasbuddy scraper!
+
+    listing_info = {}
+
     max_attempts = 5                #NOTE arbitrarily set this value. Update later!
     proxy_rotation_interval = 15    #NOTE also arbitrarily set also subject to change
 
     #TODO START HERE:
-        #3) implement the common scraping code + copy one last time to gasbuddy code
-        #4) fully implement the scraping code for CL first page & return the data to main
-        #5) add CSV writing functionality
         #6) push to AWS
         #7) implement boto3 S3 bucket writing functionality
 
@@ -62,11 +62,25 @@ def scrape_by_location(zip_code, city, current_proxy_number, proxy_count, proxy_
         try:    #
             print("attempting to connect to: " + str(listings_url))
             listings_page = requests.get(listings_url, headers = {'user-agent':listings_ua.random}, proxies = proxy_info, timeout = 5)
+            listings_soup = BeautifulSoup(listings_page.content,'lxml')
+            listings_ptags = listings_soup.find_all('p', class_ = "result-info")
+
+            for indidividual_posting in listings_ptags:
+                post_date = indidividual_posting.find('time', class_='result-date').get('datetime')
+                post_id = indidividual_posting.find('a').get('data-id')
+                post_url = indidividual_posting.find('a').get('href')
+                post_title = indidividual_posting.find('a').string
+                post_price = indidividual_posting.find('span', class_='result-price').string
+
+                #write each entry to listing_info dictionary keyed on post id
+                listing_info[post_id] = {'post_date' : post_date, 'post_url' : post_url, 'post_title' : post_title, 'post_price' : post_price}
+
+            #TODO store to listing_info - make this a return!
+
             print("connected successfully: " + str(listings_page))
 
             request_count += 1
             failed_attempts = max_attempts #NOTE set failed_attempts to max_attempts so loop terminates instead of making further requests upon successful connection!
-            #TODO scraping code goes here!
 
             break
 
@@ -84,7 +98,26 @@ def scrape_by_location(zip_code, city, current_proxy_number, proxy_count, proxy_
 
     #TODO Add empty listing/failed connection > 5 handling logic here.. (eg ZIP doesnt exist..)
     print("current proxy number: " + str(current_proxy_number) + " current request count: " + str(request_count))
-    return current_proxy_number, request_count #TODO add scraped page data output parameter here
+    return current_proxy_number, request_count, listing_info #TODO add scraped page data output parameter here
+
+def write_to_file(listing_info):
+    try:
+        with open('recent_listings.csv', 'a') as outfile:
+            out_head = ['POST_ID','POST_URL','POST_DATE','POST_TITLE','POST_PRICE']
+
+            writer = csv.DictWriter(outfile, fieldnames=out_head)
+
+            for key, values in listing_info.items():
+                writer.writerow(
+                        {   'POST_ID'   :    key,
+                            'POST_URL'  :    values['post_url'],
+                            'POST_DATE' :    values['post_date'],
+                            'POST_TITLE':    values['post_title'],
+                            'POST_PRICE':    values['post_price']}
+                )
+
+    except IOError:
+        print("I/O ERROR: file not found!")
 
 def main():
     proxy_list = import_proxy_list()
@@ -94,6 +127,6 @@ def main():
 
     #scrapes all listings by zip and write to file (in S3) for every zip in user's range of interest
     for zip_code,city in zip_list.items():
-        current_proxy_number, request_count = scrape_by_location(zip_code, city, current_proxy_number, proxy_count, proxy_list, request_count)
-        #write_to_file(listings)
+        current_proxy_number, request_count, listing_info = scrape_by_location(zip_code, city, current_proxy_number, proxy_count, proxy_list, request_count)
+        write_to_file(listing_info)
 main()
