0c4e4b967eeb55a10b400fb3b72e67d73dc7f0c1
jkMLnop <konradkarolak87@gmail.com>
2019-06-16 14:55:31 -0400

    Fixed proxy rotation logic for both 1) rotating after proxy rotation interval has been exceeded and 2) rotating proxy in event of connection error. Code is still common to both craigslist and gasbuddy scraping at this point.

--- scrape_craigslist.py
+++ scrape_craigslist.py
@@ -36,46 +36,55 @@ def import_zip_list():
     #TODO update this later to obtain range of all ZIP/cities within x mile radius of user's search
 
 def scrape_by_location(zip_code, city, current_proxy_number, proxy_count, proxy_list, request_count):
-    proxy_info = {'http': 'http://'+ proxy_list[current_proxy_number]['ip'] + ':' + proxy_list[current_proxy_number]['port']}
     listings_ua = UserAgent()
-    listings_url = 'https://' + "".join(city.split(" ")).lower() + '.craistlist.org/search/cta?postal=' + str(zip_code) #TODO Change for gasbuddy scraper!
+    listings_url = 'https://' + "".join(city.split(" ")).lower() + '.craigslist.org/search/cta?postal=' + str(zip_code) #TODO Change for gasbuddy scraper!
+    max_attempts = 5                #NOTE arbitrarily set this value. Update later!
+    proxy_rotation_interval = 15    #NOTE also arbitrarily set also subject to change
 
     #TODO START HERE:
-        #1) fix the except block, why is it not immediately rotating to the next proxy?
-        #2) fix the proxy test code - find and reference your previous implementation of danilo's code perhaps?
         #3) implement the common scraping code + copy one last time to gasbuddy code
         #4) fully implement the scraping code for CL first page & return the data to main
         #5) add CSV writing functionality
         #6) push to AWS
         #7) implement boto3 S3 bucket writing functionality
-    try:    #
-        '''
-        listings_page = requests.get(listings_url,headers={'user-agent':listings_ua.random}, proxies = proxy_info)
-        '''
-        #TODO scraping code goes here!
-        #listings_page = requests.get(listings_url,headers={'user-agent':listings_ua.random})
-        #make_soup = BeautifulSoup(listings_page.content,'lxml')
 
-        #TODO Test that proxy IP is the one being used here!!
-        ip_url = 'http://icanhazip.com'
-        proxy_ip = requests.get(ip_url, proxies = proxy_info)
-        print('proxy_ip get request: ' + proxy_ip)
+    for failed_attempts in range(max_attempts):
+        ip_url = 'http://icanhazip.com'#TODO Remove after test!
 
-    except: # If error, cycle to next proxy
-        if current_proxy_number < (proxy_count - 1):
-            current_proxy_number += 1
+        #iterate to next proxy once proxy rotation interval has been exceeded
+        if request_count >= proxy_rotation_interval:
+            if current_proxy_number < (proxy_count - 1):
+                current_proxy_number += 1
+            elif current_proxy_number == (proxy_count - 1):
+                current_proxy_number = 0
             request_count = 0
-            print("rotating to next proxy!!!!!")    #TODO Test this by doing ctrl + x  while it runs later!
-        elif current_proxy_number == (proxy_count - 1):
-            current_proxy_number = 0
+            print("proxy rotation interval exceeded, current proxy number: " + str(current_proxy_number) + " request count reset to: " + str(request_count))
+
+        proxy_info = {'http': 'http://'+ proxy_list[current_proxy_number]['ip'] + ':' + proxy_list[current_proxy_number]['port']}
+
+        try:    #
+            '''
+            listings_page = requests.get(listings_url,headers={'user-agent':listings_ua.random}, proxies = proxy_info)
+            '''
+            proxy_ip = requests.get(ip_url, proxies = proxy_info, timeout = 5)  #TODO Remove after test!
+            request_count += 1
+            failed_attempts = 0 #NOTE might change this if we scrape deeper and fail on subsequent pages..
+            #TODO scraping code goes here!
+
+        except: # If error, cycle to next proxy and reset request count for that proxy to 0 TODO make more specific error conditions!!
+            if current_proxy_number < (proxy_count - 1):
+                current_proxy_number += 1
+            elif current_proxy_number == (proxy_count - 1):
+                current_proxy_number = 0
+
             request_count = 0
-            print("rotating to first proxy!!!!!")    #TODO Test this by doing ctrl + x  while it runs later!
+            failed_attempts += 1
+            print("connection error encountered, rotating to proxy number: " + str(current_proxy_number))
 
-        #TODO Possibly add proxy deletion capacity
+            #TODO Possibly add proxy deletion capacity here
 
-    print("proxy_info: " + str(proxy_info))
-
-    return(current_proxy_number, proxy_count, proxy_list, request_count) #TODO RETURN ALL DATA FOR THAT PAGE + ALL VARIABLES RELATING TO PROXY!
+    print("current proxy number: " + str(current_proxy_number) + " current request count: " + str(request_count))
+    return current_proxy_number, request_count #TODO add scraped page data output parameter here
 
 def main():
     proxy_list = import_proxy_list()
@@ -83,24 +92,9 @@ def main():
     proxy_count = len(proxy_list)
     current_proxy_number  = 0    #TODO: Can I initialize in the loop?
     request_count = 0
-    proxy_rotation_interval = 15
 
+    #scrapes all listings by zip and write to file (in S3) for every zip in user's range of interest
     for zip_code,city in zip_list.items():
-
-        listings = scrape_by_location(zip_code, city, current_proxy_number, proxy_count, proxy_list, request_count)
+        current_proxy_number, request_count = scrape_by_location(zip_code, city, current_proxy_number, proxy_count, proxy_list, request_count)
         #write_to_file(listings)
-
-        request_count += 1
-        #print("request count: " + str(request_count))
-        #print("current proxy number: " + str(current_proxy_number)  + " ip: " + str(proxy_list[current_proxy_number]['ip']) + " port: " + str(proxy_list[current_proxy_number]['port']))
-
-        #iterate to next proxy
-        if request_count >= proxy_rotation_interval:
-            if current_proxy_number < (proxy_count - 1):
-                current_proxy_number += 1
-                request_count = 0
-            elif current_proxy_number == (proxy_count - 1):
-                current_proxy_number = 0
-                request_count = 0
-
 main()
