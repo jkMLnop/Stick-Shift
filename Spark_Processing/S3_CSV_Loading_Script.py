e3e713575b8e4d01912b25e9d453ed9be0dea417
Ubuntu <ubuntu@ip-10-0-0-10.ec2.internal>
2019-06-19 20:22:04 +0000

    updated loading script to write to db

--- Spark_Processing/S3_CSV_Loading_Script.py
+++ Spark_Processing/S3_CSV_Loading_Script.py
@@ -1,6 +1,3 @@
-import findspark
-#findspark.init('/usr/local/spark')
-
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
 
@@ -9,6 +6,21 @@ sc = SparkContext('local','building a fuel price data warehouse')#conf=conf)
 sqlContext = SQLContext(sc)
 
 # Load data from CSV
-file_location = "/Desktop/Web_Scraping/Scraping_Backups/recent_listings.csv"
+file_location = "/home/ubuntu/Stick-Shift/Web_Scraping/recent_listings.csv"
 df = sqlContext.read.format("CSV").option("inferSchema", True).option("header", True).load(file_location)
-display(df.take(5))
+df.show()
+
+df.write.format("jdbc").options( 
+    url="jdbc:postgresql://ec2-18-206-181-71.compute-1.amazonaws.com:5432/fuel_consumption", 
+    dbtable="car_listings", 
+    driver="org.postgresql.Driver", 
+    user = "postgres", 
+    password="postgres", 
+    mode = "append").save()
+
+
+
+
+
+
+
